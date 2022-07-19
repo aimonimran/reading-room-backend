229a0d902a66418591f7ca2a43fcd9b3d0fc9cc9
aimonimran <aimon.imran@gmail.com>
2022-07-19 15:10:33 +0500

    final commits

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { User } = require('../models/user');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,30 @@
-const { User, validate } = require('../models/user');
-const express = require('express');
+const { User, validate } = require("../models/user");
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-router.get('/', async (req, res) => {
-    const users = await User.find().select('-password');
-    res.send(users);
-})
+router.get("/", async (req, res) => {
+  const users = await User.find().select("-password");
+  res.send(users);
+});
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    
-        let user = new User({
-            username: req.body.username,
-            password: hashedPassword
-        })
-        user = await user.save();
-        res.status(201).send(`Welcome, ${user.username}!`);
-    } catch(err) {
-        res.status(500).send();
-    }
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    let user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    user = await user.save();
+    res.status(201).send(`Welcome, ${user.username}!`);
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
